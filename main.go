a60901610a6b3c8c2ac7a0dfd7a3be65f1715450
shibh308 <s17097@tokyo.kosen-ac.jp>
2020-09-04 17:23:28 +0900

    [change]directory

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/shibh308/cursorfuncinfo"
+	"github.com/shibh308/cursorfuncinfo/run"
 	"os"
 )
 
 func main() {
-	str, err := cursorfuncinfo.Run()
+	str, err := run.Run()
 	if err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
--- run/run.go
+++ run/run.go
@@ -1,4 +1,4 @@
-package cursorfuncinfo
+package run
 
 import (
 	"context"
